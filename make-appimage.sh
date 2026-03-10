115cd8e72506003c0eb242e882eca0d19a6c981d
Link4Electronics <link4electronics@gmail.com>
2026-03-10 04:17:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,6 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/256x256/apps/sheepshaver.png
 export DESKTOP=/usr/share/applications/sheepshaver.desktop
 export STARTUPWMCLASS=
 export DEPLOY_OPENGL=1
